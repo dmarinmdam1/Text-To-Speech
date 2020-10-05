984f9dde43561e75eac71ad7cb89133ef8b1e963
dmarinmdam1 <dmarinmdam1@gmail.com>
2020-10-05 22:40:06 +0200

    Working and customized

---
+++ lib/MyHomePage.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_text_to_speech/flutter_text_to_speech.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  VoiceController _voiceController;
+
+  String text = '';
+
+  @override
+  void initState() {
+    _voiceController = FlutterTextToSpeech.instance.voiceController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _voiceController.stop();
+  }
+
+  _playVoice() {
+    _voiceController.init().then((_) {
+      if (text != '') {
+        _voiceController.setLanguage("es-ES");
+        _voiceController.speak(
+          text,
+          VoiceControllerOptions(),
+        );
+      }
+    });
+  }
+
+  _stopVoice() {
+    _voiceController.stop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.85,
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Introduce texto para reproducir en voz',
+                ),
+                onChanged: (TextField_text) {
+                  text = TextField_text;
+                },
+              ),
+            ),
+            SizedBox(
+              height: 60,
+            ),
+            RaisedButton(
+              onPressed: _playVoice,
+              color: Colors.green,
+              child: Text('Reproducir texto en voz'),
+            ),
+            RaisedButton(
+              onPressed: _stopVoice,
+              color: Colors.red,
+              child: Text('Parar reproducción'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_text_to_speech/flutter_text_to_speech.dart';
+import 'MyHomePage.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,84 +11,22 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Text To Speech',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: MaterialColor(0xFF880E4F, color),
       ),
-      home: MyHomePage(title: 'Text To Speech'),
+      home: MyHomePage(title: 'Conversor de texto a voz'),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  VoiceController _voiceController;
-
-  String text = 'This is an example tutorial of using text to speech in a flutter application! The example is provided on fluttercentral website.';
-
-  @override
-  void initState() {
-    _voiceController = FlutterTextToSpeech.instance.voiceController();
-
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _voiceController.stop();
-  }
-
-  _playVoice() {
-    _voiceController.init().then((_) {
-      _voiceController.speak(
-        text,
-        VoiceControllerOptions(),
-      );
-    });
-  }
-
-  _stopVoice() {
-    _voiceController.stop();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              text,
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            RaisedButton(
-              onPressed: _playVoice,
-              color: Colors.blue,
-              child: Text('Play Voice'),
-            ),
-            RaisedButton(
-              onPressed: _stopVoice,
-              color: Colors.blue,
-              child: Text('Stop Voice'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+  final Map<int, Color> color = {
+    50: Color.fromRGBO(255, 0, 255, .1),
+    100: Color.fromRGBO(255, 0, 255, .2),
+    200: Color.fromRGBO(255, 0, 255, .3),
+    300: Color.fromRGBO(255, 0, 255, .4),
+    400: Color.fromRGBO(255, 0, 255, .5),
+    500: Color.fromRGBO(255, 0, 255, .6),
+    600: Color.fromRGBO(255, 0, 255, .7),
+    700: Color.fromRGBO(255, 0, 255, .8),
+    800: Color.fromRGBO(255, 0, 255, .9),
+    900: Color.fromRGBO(255, 0, 255, 1),
+  };
 }
